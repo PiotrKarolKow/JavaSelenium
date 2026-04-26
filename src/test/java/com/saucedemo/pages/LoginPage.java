c1b464cf642107f945195ba809fd4909d616943e
PiotrKarolKow <piotr.kowalczyk1210@gmail.com>
2026-04-26 12:23:16 +0000

    testytestytestytesty

--- src/test/java/com/saucedemo/pages/LoginPage.java
+++ src/test/java/com/saucedemo/pages/LoginPage.java
@@ -21,6 +21,8 @@ public class LoginPage {
     private WebElement password;
     @FindBy(id = "login-button")
     private WebElement button;
+    @FindBy(css = ".error-message-container")
+    private WebElement errorMessage;
 
     public LoginPage(WebDriver driver) {
         this.driver = driver;
@@ -39,5 +41,18 @@ public class LoginPage {
         password.sendKeys(passwd);
         button.click();
         return new ProductsPage(driver);
+    }
+
+    public LoginPage loginInvalid(String username, String passwd) {
+        this.username.clear();
+        this.username.sendKeys(username);
+        password.clear();
+        password.sendKeys(passwd);
+        button.click();
+        return this;
+    }
+
+    public String getErrorMessage() {
+        return wait.until(ExpectedConditions.visibilityOf(errorMessage)).getText();
     }
 }
