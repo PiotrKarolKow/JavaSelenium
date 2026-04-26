19ead5aec9d00d73ee78d48f99b54fc29c41e661
PiotrKarolKow <piotr.kowalczyk1210@gmail.com>
2026-04-26 12:11:07 +0000

    pop test

--- src/test/java/com/saucedemo/tests/LoginTest.java
+++ src/test/java/com/saucedemo/tests/LoginTest.java
@@ -1,13 +1,14 @@
 package com.saucedemo.tests;
 
 import com.saucedemo.pages.LoginPage;
+import com.saucedemo.tests.SauceDemoBaseTest;
 import org.testng.annotations.Test;
 
-public class LoginTest {
+public class LoginTest extends SauceDemoBaseTest {
 
     @Test
-    public void loginValid(){
-        LoginPage loginPage = new LoginPage();
-        loginPAge.open(baseUrl);
+    public void loginValid() {
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.open(baseUrl);
     }
 }
